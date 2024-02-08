1bbb645111e505f73e33d04c171e739daef870a9
farad-tech <f1378321.karami@gmail.com>
2024-02-08 12:18:56 +0330

    exit button hover effect changed

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -7,7 +7,7 @@
     id="1"
   />
 
-  <div class="card hover:shadow-lg hover:-translate-x-1">
+  <div class="card hover:shadow-sm hover:-translate-x-1">
     <ArrowLeftEndOnRectangleIcon class="h-6 w-6 text-danger" />
     <p class="text-danger">خروج از حساب کاربری</p>
   </div>
