70ef0018b8644c3644e91632329816548f93c885
farad-tech <f1378321.karami@gmail.com>
2024-02-14 16:05:11 +0330

    Calendar input basics implemented

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
+import { VueElement, createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router.js'
+import Vue3PersianDatetimePicker from 'vue3-persian-datetime-picker'
 
 const app = createApp(App)
 app.use(router)
+app.component('DatePicker', Vue3PersianDatetimePicker)
 app.mount('#app')
--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -3,29 +3,29 @@
     دریافت نوبت آرایشگاه آث
   </TitleText>
 
-  <StoreProf
-    avatar="https://ui-avatars.com/api/?name=ف&color=FFFFFF&background=C70039"
-    name="آرایشگاه آث"
-    tagline="آرایشگاه مردانه"
-    phone="09051873547"
-    id="1"
-    address="گیلان، تالش، میدان امام خمینی جنب درمانگاه شفا"
-    :editable="false"
+  <StoreProf avatar="https://ui-avatars.com/api/?name=ف&color=FFFFFF&background=C70039" name="آرایشگاه آث"
+    tagline="آرایشگاه مردانه" phone="09051873547" id="1" address="گیلان، تالش، میدان امام خمینی جنب درمانگاه شفا"
+    :editable="false" />
 
-  />
-
-  <div class="text-danger">
-    محل تنظیم زمان
-  </div>
+  <date-picker v-model="date" type="datetime" :min="currentDate"  :disable="disabledDates"/>
 
   <div class="text-center my-5 bg-primary text-white rounded-xl py-2 shadow-sm click">
     دریافت نوبت
   </div>
-
 </template>
 
 <script setup>
-  import TitleText from '../components/TitleText.vue';
-  import StoreProf from '../components/Store.vue';  
+
+import { ref } from 'vue';
+import TitleText from '../components/TitleText.vue';
+import StoreProf from '../components/Store.vue';
+import fixNumbers from '../custom-javascript/fixNumbers';
+
+const date = ref('');
+
+var currentDate = new Date();
+currentDate = fixNumbers(currentDate.toLocaleString('fa-IR'));
+
+var disabledDates = ['Friday'];
 
 </script>
