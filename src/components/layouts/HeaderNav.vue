7067cdc0736ef1122a5b9e2469c3ad54bf6cdebb
farad-tech <f1378321.karami@gmail.com>
2024-06-25 13:50:53 +0330

    front-end pages completed

--- src/components/layouts/HeaderNav.vue
+++ src/components/layouts/HeaderNav.vue
@@ -1,21 +1,38 @@
 <script setup>
+import { ArrowLeftStartOnRectangleIcon } from '@heroicons/vue/24/outline';
+import Swal from 'sweetalert2'
 
+const logout = () => {
+
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You are about to cancel appointment.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success"
+      });
+    }
+  });
+
+}
 </script>
 
 <template>
   <div class="navbar bg-base-100">
     <div class="flex-1">
-      <img src="" alt="logo"/>
+      <!-- <img src="" alt="logo"/> -->
+      Nobatdaram
     </div>
     <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z">
-          </path>
-        </svg>
-      </button>
+      <ArrowLeftStartOnRectangleIcon @click="logout" class="w-5 h-5 text-red-400" />
     </div>
   </div>
 </template>
--- src/views/Panel/Profile.vue
+++ src/views/Panel/Profile.vue
@@ -3,20 +3,43 @@ import HeaderNav from '@/components/layouts/HeaderNav.vue'
 import FooterNav from '@/components/layouts/FooterNav.vue'
 import Avatar from '@/components/elements/Avatar.vue'
 import { ref } from 'vue';
-import { PencilIcon } from '@heroicons/vue/24/solid';
+import { XCircleIcon } from '@heroicons/vue/24/solid';
 import ProfileHeader from '@/components/layouts/ProfileHeader.vue';
+import Swal from 'sweetalert2'
 
-const activeMenu = ref('Appointments');
+const activeMenu = ref('Yours');
 
 const Menus = [
-  'Appointments',
-  'History',
+  'Yours',
+  'Customers',
 ];
 
 const changeMenu = (menuName) => {
 
   activeMenu.value = menuName;
 
+}
+
+const cancelAppointment = () => {
+
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You are about to cancel appointment.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success"
+      });
+    }
+  });
+  
 }
 
 </script>
@@ -28,7 +51,10 @@ const changeMenu = (menuName) => {
 
     <ProfileHeader />
 
-    <div class="my-10 text-center">
+    
+    <div class="text-center mt-10">Appointments</div>
+
+    <div class="my-5 text-center text-xs">
       <ul class="menu menu-horizontal bg-base-200 rounded-box">
         <li v-for="Menu in Menus">
           <a @click="changeMenu(Menu)" :class="{ 'active': activeMenu == Menu }">{{ Menu }}</a>
@@ -36,8 +62,23 @@ const changeMenu = (menuName) => {
       </ul>
     </div>
 
-    <div>
+    <!-- <div>
       <div v-for="i in 1" class="skeleton w-full h-12 mb-2"></div>
+    </div> -->
+
+    <div class="block rounded-xl bg-base-300 hover:bg-base-200 w-full shadow-xl p-2 py-3">
+      <div class="flex justify-between items-center">
+        <div class="flex items-center">
+          <Avatar class="w-12 me-2" />
+          <div>
+            <p>name</p>
+            <p class="text-sm">2024 - 06 - 27</p>
+          </div>
+        </div>
+        <div>
+          <XCircleIcon @click="cancelAppointment" class="cursor-pointer w-8 text-red-300" />
+        </div>
+      </div>
     </div>
 
   </div>
